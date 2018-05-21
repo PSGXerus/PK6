22b4026949d882c776577a7deca787e0e31ee49d
Jan Siegmund <jsiegmun@hm.edu>
2018-05-21 19:27:56 +0200

    src: bootloader: Added debug script
    Added a script for testing the plymouth
    splash screen. It is an edited version
    of the provided script from _khAttAm_.
    
    Signed-off-by: Jan Siegmund <jsiegmun@hm.edu>

---
+++ src/bootloader/test_plymouth.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## Preview Plymouth Splash ##
+##      by _khAttAm_       ##
+##    www.khattam.info     ##
+##    License: GPL v3      ##
+
+chk_root () {
+  if [ ! $( id -u ) -eq 0 ]; then
+    echo Must be run as root
+    exit
+  fi
+}
+
+chk_root
+
+DURATION=$1
+if [ $# -ne 1 ]; then
+DURATION=5
+fi
+
+plymouthd
+plymouth --show-splash
+sleep $DURATION;
+plymouth message --text="hello world, this is plymouth"
+sleep $DURATION;
+plymouth quit
