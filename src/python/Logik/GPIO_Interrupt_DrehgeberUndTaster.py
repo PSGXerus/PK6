db8fef99c5c4a10e0708d97b13486bc0a04df65e
Jan Siegmund <jsiegmun@hm.edu>
2018-06-02 01:05:33 +0000

    src: python: Logik: Squashed the two encoder callbacks in one
    Also cleaned the python script.
    
    Signed-off-by: Jan Siegmund <jsiegmun@hm.edu>

--- src/python/Logik/GPIO_Interrupt_DrehgeberUndTaster.py
+++ src/python/Logik/GPIO_Interrupt_DrehgeberUndTaster.py
@@ -22,85 +22,64 @@ value = GPIO.input(tasterPin1)
 oldValue = 0
 counter = 0
 
-def call1(tasterPin1):
+#Konstanten
+DIR_NONE = 0x0
+DIR_CW = 0x10
+DIR_CCW = 0x20
+
+R_START = 0x0
+R_CW_FINAL = 0x1
+R_CW_BEGIN = 0x2
+R_CW_NEXT = 0x3
+R_CCW_BEGIN = 0x4
+R_CCW_FINAL = 0x5
+R_CCW_NEXT = 0x6
+
+#Full Table Deklaration
+ttable = [[R_START, R_CW_BEGIN, R_CCW_BEGIN, R_START],
+         [R_CW_NEXT, R_START, R_CW_FINAL, R_START|DIR_CW],
+         [R_CW_NEXT, R_CW_BEGIN, R_START, R_START],
+         [R_CW_NEXT, R_CW_BEGIN, R_CW_FINAL, R_START],
+         [R_CCW_NEXT, R_START, R_CCW_BEGIN, R_START],
+         [R_CCW_NEXT, R_CCW_FINAL, R_START, R_START|DIR_CCW],
+         [R_CCW_NEXT, R_CCW_FINAL, R_CCW_BEGIN, R_START]]
+
+#State Vorinitialisieren
+state = R_START
+
+def call1(channel):
     global counter
     counter = counter +1
     print (counter)
 
-def call2(tasterPin2):
+def call2(channel):
     global counter
     counter = counter -1
     print (counter)
 
-def call3(tasterPin3):
+def call3(channel):
     global counter
     counter = 0
     print (counter)
 
 
-def call4(drehgeberPin1):
-    GPIO.remove_event_detect(drehgeberPin2)
-    state = R_START
-    x = True
-    while(x):
-        pinstate = ((GPIO.input(drehgeberPin2) << 1)| GPIO.input(drehgeberPin1))
-        state = ttable[state & 0xf][pinstate]
-        if state ==  DIR_CW:
-            print ("Rechts")
-            x = False
-        elif state == DIR_CCW:
-            print ("Links")
-            x = False
-    GPIO.add_event_detect(drehgeberPin2, GPIO.BOTH, callback = call5, bouncetime = 200)
-
-def call5(drehgeberPin2):
-    GPIO.remove_event_detect(drehgeberPin1)
-    state = R_START
-    x= True
-    while(x):
-        pinstate = ((GPIO.input(drehgeberPin2) << 1)| GPIO.input(drehgeberPin1))
-        state = ttable[state & 0xf][pinstate]
-        if state ==  DIR_CW:
-            print ("Rechts")
-            x = False
-        elif state == DIR_CCW:
-            print ("Links")
-            x = False
-    GPIO.add_event_detect(drehgeberPin1, GPIO.BOTH, callback = call4, bouncetime = 200)
+def call4(channel):
+    global state
+    pinstate = ((GPIO.input(drehgeberPin2) << 1)| GPIO.input(drehgeberPin1))
+    state = ttable[state & 0xf][pinstate]
+    if state ==  DIR_CW:
+        print ("Rechts")
+    elif state == DIR_CCW:
+        print ("Links")
 
 GPIO.add_event_detect(tasterPin1, GPIO.FALLING, callback = call1, bouncetime = 200)
 GPIO.add_event_detect(tasterPin2, GPIO.FALLING, callback = call2, bouncetime = 200)
 GPIO.add_event_detect(tasterPin3, GPIO.FALLING, callback = call3, bouncetime = 200)
 
-GPIO.add_event_detect(drehgeberPin1, GPIO.BOTH, callback = call4, bouncetime = 200)
-GPIO.add_event_detect(drehgeberPin2, GPIO.BOTH, callback = call5, bouncetime = 200)
+GPIO.add_event_detect(drehgeberPin1, GPIO.BOTH, callback = call4)
+GPIO.add_event_detect(drehgeberPin2, GPIO.BOTH, callback = call4)
 
 try:
-    #Konstanten
-    DIR_NONE = 0x0
-    DIR_CW = 0x10
-    DIR_CCW = 0x20
-
-    R_START = 0x0
-    R_CW_FINAL = 0x1
-    R_CW_BEGIN = 0x2
-    R_CW_NEXT = 0x3
-    R_CCW_BEGIN = 0x4
-    R_CCW_FINAL = 0x5
-    R_CCW_NEXT = 0x6
-
-    #Full Table Deklaration
-    ttable = [[R_START, R_CW_BEGIN, R_CCW_BEGIN, R_START],
-             [R_CW_NEXT, R_START, R_CW_FINAL, R_START|DIR_CW],
-             [R_CW_NEXT, R_CW_BEGIN, R_START, R_START],
-             [R_CW_NEXT, R_CW_BEGIN, R_CW_FINAL, R_START],
-             [R_CCW_NEXT, R_START, R_CCW_BEGIN, R_START],
-             [R_CCW_NEXT, R_CCW_FINAL, R_START, R_START|DIR_CCW],
-             [R_CCW_NEXT, R_CCW_FINAL, R_CCW_BEGIN, R_START]]
-
-    #State Vorinitialisieren
-    state = R_START
-
     while True:
         time.sleep(1)
 
